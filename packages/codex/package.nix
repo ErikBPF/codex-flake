939fc3606831c773ab23c6781c70f2aa3728f862
ErikBPF <26701869+ErikBPF@users.noreply.github.com>
2026-09-25 14:39:58 +0000

    chore: update packaged upstream

--- packages/codex/package.nix
+++ packages/codex/package.nix
@@ -11,13 +11,13 @@
   versionCheckHook,
   installShellCompletions ? stdenvNoCC.buildPlatform.canExecute stdenvNoCC.hostPlatform,
 }: let
-  version = "0.156.1";
+  version = "0.157.0";
 
   hashes = {
-    x86_64-linux = "sha256-i3EVIL7d84VGe42k0sk3NmN8a6HkaBHPDYYGt8SQtvY=";
-    aarch64-linux = "sha256-/dR+1qreA2B5b9P2+VpFCW8yfBXhnoxzOfncVjMEF4Y=";
-    x86_64-darwin = "sha256-YY281VQZ+gQYcfd3oUsQfOs/4tM574HiHmq1N0Qg3HE=";
-    aarch64-darwin = "sha256-/qQvliUJHwEeOPBZ2pdNUuV7oxgxZIuxx/Cxo4X95Uc=";
+    x86_64-linux = "sha256-BC+FHqP8EIPEUVdSBSCUT8eQYytT68WA/JjqylWGKiU=";
+    aarch64-linux = "sha256-wcNr6rC09yd5rfU7qenklL98v75PNQagj/Z6JPXwDQg=";
+    x86_64-darwin = "sha256-UcyJ0yFFweXdSqlnC4j6y+uWe6FHBHBoCuhTdvPjQKA=";
+    aarch64-darwin = "sha256-l4Cfkcs1XlVIDNehJvmtJLt7FiIiUV4wKGvKxvupSs0=";
   };
 
   triples = {
